97975addf8dfbdd638ef5d4be37fae90269ccd01
Daniil Borovkov <daniilborowkow@gmail.com>
2019-10-24 15:20:03 +0600

    Wip creating styling for block

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -56,9 +56,9 @@ const requireDir = require("require-dir"),
             watch: "./src/img/svg/*.svg"
         },
         fonts: {
-            src: "./src/fonts/**/*.{woff,woff2}",
+            src: "./src/fonts/**/*.{woff,woff2,ttf,eot}",
             dist: "./dist/fonts/",
-            watch: "./src/fonts/**/*.{woff,woff2}"
+            watch: "./src/fonts/**/*.{woff,woff2,ttf,eot}"
         },
         favicons: {
             src: "./src/img/favicon/*.{jpg,jpeg,png,gif}",
